6ff01f164505a2c9d8868f8b76216e2b5a616590
Cameron Clough <cameronjclough@gmail.com>
2020-08-07 19:57:34 +0100

    feat: add build timestamp

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <Avatar />
   <HelloWorld msg="Hello, I'm Cameron" />
+  <span class="timestamp">Last updated: {{ buildTimestamp }}</span>
 </template>
 
 <script>
@@ -13,5 +14,24 @@ export default {
     Avatar,
     HelloWorld,
   },
+  computed: {
+    buildTimestamp() {
+      const env = '2020-08-07T17:16:00+00:00' || process.env.VUE_APP_BUILD_TIMESTAMP;
+      if (env) {
+        return env.substring(0, env.length - 6);
+      }
+      const result = new Date().toISOString();
+      return result.substring(0, result.length - 5);
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.timestamp {
+  font-size: 14px;
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+}
+</style>
