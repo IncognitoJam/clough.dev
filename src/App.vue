5f7201f6326f243d7bb2ccae878ec4f33e1bebed
Cameron Clough <cameronjclough@gmail.com>
2020-10-10 20:55:47 +0100

    feat: implement SSHKey component

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
     <BitcoinMiner />
     <BuildTimestamp />
     <v-spacer />
-    <span class="caption" @click="copySSHKey">Copy SSH key</span>
+    <SSHKey />
   </section>
 </template>
 
@@ -27,6 +27,7 @@ import BitcoinMiner from './components/BitcoinMiner.vue';
 import BuildTimestamp from './components/BuildTimestamp.vue';
 import HackerTyper from './components/HackerTyper.vue';
 import SocialLinks from './components/SocialLinks.vue';
+import SSHKey from './components/SSHKey.vue';
 
 export default {
   name: 'App',
@@ -36,11 +37,7 @@ export default {
     BuildTimestamp,
     HackerTyper,
     SocialLinks,
-  },
-  methods: {
-    async copySSHKey() {
-      await navigator.clipboard.writeText('ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCr7F7+EBogt2ELrLAbzc1IL7xORpP1XcuTz3bz+y5kW3l72sGrfFqUusc0LlpJFR3rfTLHJUpg8//iVoTHpxDDOtpAVT7j2X7G4qITllQGZz/Y3lafyipO+aw04MD06z0eSVOejlPlYrp57yMyTCTYTFuYmW4LM2ysgu8ZYdnn46zI/UdPw03jFV3NwsKtpBtr9l2HxRWrX4i7fPlaFALAdaMzILw/LTAIPd/YXP/A1/e/JJuiFDUylbY7B0ryPc54ftosF36IQncCN6kDQrmWtQyadlaJ9Yj/pEeFMGPCp1NniSUe2UPxSvTfa9bcTtn9eLA1k+2egFYvpiuE570IdW/yi/+LvAsSdlAcC7bspdL2IBzNxDfgBHYx0jogUqkv5KUQ6pA1OlrDcWM0W/ZGjkQWEeeuCRNVxz3YSQUtunUU45O6gZuxpOaCMCNN7aNhnm3Cr0sgT2snkj4YoejIK+GPdxCFlVPwEHA2Pvt28UcTHdvgQOK5KXBrrikJZWc= Pixel4XL');
-    }
+    SSHKey,
   },
 };
 </script>
@@ -60,6 +57,12 @@ export default {
   .build-timestamp {
     position: absolute;
     right: 0;
+  }
+
+  .ssh-key {
+    position: absolute;
+    right: 0;
+    bottom: 32px;
   }
 }
 </style>
